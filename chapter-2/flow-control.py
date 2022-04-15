1ec5035089e09063587a8c29a5c9874e23491a13
John Walsh <jwalsh481@gmail.com>
2022-04-15 20:06:46 +0100

    Add chapter 2 - flow control, booleans
    Added chapter 2 folder with flow-control.py, includes boolean
    values, boolean operators and comparison operators.

---
+++ chapter-2/flow-control.py
@@ -0,0 +1,31 @@
+# Python flow control using booleans, comparison operators, conditional statements and loops.
+
+# Boolean values
+eggs = True         # Evaluates to True
+spam = False        # Evaluates to False
+
+# Boolean Operators (and, or, not)
+print(eggs and eggs)    # Prints True
+print(eggs and spam)    # Prints False
+print(spam and spam)    # Prints False
+
+print(eggs or eggs)     # Prints True
+print(eggs or spam)     # Prints True
+print(spam or spam)     # Prints False
+
+print(not eggs)         # Prints False
+print(not spam)         # Prints True
+
+# Comparison operators
+print(11 == 11)         # Prints True
+print(34 == 23)         # Prints False
+
+print(2 != 4)           # Prints True
+print(2 != 2)           # Prints False
+
+print(56 > 10)          # Prints True
+print(53 > 61)          # Prints False
+
+
+
+
