d51c7aa07448e9d8a45d4379f1d5ef5fbcef74b6
John Walsh <jwalsh481@gmail.com>
2022-04-15 17:49:59 +0100

    Add initial commit, chapter 1 operators
    Added info on basic python operators, included examples of each.

---
+++ chapter-1/python-basics-operators.py
@@ -0,0 +1,30 @@
+# Basic Expressions and Operations
+
+# Exponent
+exp = 4 ** 2        # Evaluates to 16
+
+# Modulus/remainder
+modulus = 11 % 2    # Evaluates to 1
+
+# Integer division/floored quotient
+int_div = 32 // 8   # Evaluates to 4 
+
+# Division
+div = 32 / 8        # Evaluates to 4.0
+
+# Multiplication
+mult = 2 * 5        # Evaluates to 10
+
+# Subtraction
+subtract = 44 - 20  # Evalueates to 24
+
+# Addition
+addition = 3 + 4    # Evaluates to 7
+
+print(exp)
+print(modulus)
+print(int_div)
+print(div)
+print(mult)
+print(subtract)
+print(addition)
